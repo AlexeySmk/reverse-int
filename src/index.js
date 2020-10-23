76b39202aee83fb0d50835a88dde9d8907fa7fd6
AlexeySmk <temp.mail.93@list.ru>
2020-10-23 17:39:16 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let nToString = String(Math.abs(n));
+    let result = "";
+    for (let i = nToString.length - 1; i >= 0; i--) {
+
+        result += nToString[i];
+
+    }
+
+    return +result;
+
 }
